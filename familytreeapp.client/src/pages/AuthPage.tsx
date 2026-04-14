c7ffa5df82fc9ad7e0fbe32e04eb0dd46163b6b3
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2026-04-14 22:01:22 +0200

    add login to auth page

--- familytreeapp.client/src/pages/AuthPage.tsx
+++ familytreeapp.client/src/pages/AuthPage.tsx
@@ -1,9 +1,10 @@
 import { Container, Typography } from '@mui/material';
-
+import LoginFormComponent from '@/components/auth/LoginFormComponent';
 export default function AuthPage() {
   return (
     <Container maxWidth="sm" sx={{ mt: 4, mb: 4 }}>
-      <Typography variant="h4">Login / Register</Typography>
+          <Typography variant="h4">Login / Register</Typography>
+          <LoginFormComponent />
     </Container>
   );
 }
